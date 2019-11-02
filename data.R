8d66783967a734efbc158ff14cd385ab85baf1fb
devinkwok <devin@devin-y580.localdomain>
2019-11-01 23:26:52 -0600

    REFACTOR/ADD: Refactor fit_linear_models, generate function signatures for statistics.
    Refactored fit_linear_models to pull out model generation into build_formula_string, and
    model testing and k-fold data partitioning into k_fold_cv_train_lm. This allows multiple
    models to be trained on same parameters and dataset via k-folds. Added function signatures
    for test_lm, get_individual_statistics, get_paired_statistics, and generate_lm_statistics
    most of which are incomplete. These will be filled out to finish data.R. Program flow is
    as follows: linear models are generated and tested in generate_lms, then statistics are
    gathered and compiled into a dataframe by generate_lm_statistics. The data is saved to
    file and this is used to train the meta-model in a separate file.
    
    Also added /lib/ folder for packaging libraries directly with the source. Ran into some
    issues with locally installed version of R and package car, will need to investigate
    before adding local copies of necessary libraries.

--- data.R
+++ data.R
@@ -1,7 +1,12 @@
- # functions for cleaning and moving data
+# functions for cleaning and moving data
+# also building dataset for meta-model
 
 # traceback errors for debugging
 options(error=function()traceback(2))
+
+# need car library for vif function
+# TODO: need R version 3.05 or higher to get most recent version of car
+# library("car", lib.loc="lib")
 
 DATA_PATH = "datasets"
 MAX_DATASETS = 1  # to limit computation time while debugging
@@ -10,6 +15,7 @@ MIN_COLUMNS = 2
 MAX_COLUMNS = 15
 MIN_ROWS = 100
 MAX_ROWS = 10000
+NUM_FOLDS = 1  # for k fold cross validation, set to 5 later
 
 # checks if dataframe is valid to include
 is_valid_data = function(dataframe) {
@@ -108,19 +114,55 @@ combination_matrix = function(size) {
     return(expand.grid(rep(list(c(0:1)), size)))
 }
 
-# fits linear models for every combination of parameters to a dataset
-# ASSUMPTION: first column of dataframe is used as response variable
-# FIXME: incomplete
-fit_linear_models = function(dataframe) {
-    #TODO: k-fold cross validation
-    # get dataset columns
-    columns = colnames(dataframe)
-    num_predictors = length(columns) - 1
-
-    # print(columns)
-    
+# refactor this out of fit_linear_models
+build_formula_string = function(inclusion_matrix_row, columns) {
     response_var = columns[1]
     predictors = columns[-1] # this removes column 1 from the list
+    num_included = sum(inclusion_matrix_row)
+    
+    included_predictors = vector("list", num_included)
+    k = 1  # because R needs index variables
+    
+    # iterate through columns for each predictor
+    for (j in 1:length(predictors)) {
+        if (inclusion_matrix_row[j] == 1) {
+            included_predictors[k] = predictors[j]
+            k = k + 1
+        }
+    }
+
+    # if there are no predictors, add a constant term
+    formula_string = "1"
+    if (length(included_predictors) > 0) {
+        formula_string = paste(included_predictors, collapse="+")
+    }
+    formula_string = paste(response_var, "~", formula_string)
+    return(formula_string)
+}
+
+# TODO: implement k-fold and validate each
+k_fold_cv_train_lm = function(inclusion_matrix_row, dataframe, num_folds) {
+    # get dataset columns
+    columns = colnames(dataframe)
+    models = list()
+    test_results = list()
+
+    formula_string = build_formula_string(inclusion_matrix_row, columns)
+
+    for (i in 1:num_folds) {
+        train_data = dataframe
+        # TODO: create proper test subset
+        test_data = dataframe
+        model = lm(formula=formula_string, data=train_data)
+        models[[1]] = model
+        test_results[[1]] = test_lm(model, test_data)
+    }
+    return(c(models, test_results))
+}
+
+# fits linear models for every combination of parameters to a dataset
+# ASSUMPTION: first column of dataframe is used as response variable
+fit_linear_models = function(dataframe, num_folds) {
 
     # go through all combinations of predictors
     # TODO: if there are too many predictors it is better to stochastically sample this space
@@ -128,39 +170,56 @@ fit_linear_models = function(dataframe) {
     # use a matrix of 0 and 1 to indication inclusion/exclusion of predictor
     # rows are for different linear models
     # columns are the inclusion/exclusion of that column in the dataframe
-    inclusion_matrix = combination_matrix(num_predictors)
+    # number of predictors is number of columns - 1
+    inclusion_matrix = combination_matrix(length(dataframe) - 1)
 
-    models = vector("list", nrow(inclusion_matrix))
+    model_data = vector("list", nrow(inclusion_matrix))
 
     # iterate through rows for each model
     for (i in 1:length(inclusion_matrix[,1])) {
-        
-        num_included = sum(inclusion_matrix[i,])
-        
-        included_predictors = vector("list", num_included)
-        k = 1  # because R needs index variables
-        
-        # iterate through columns for each predictor
-        for (j in 1:num_predictors) {
-            if (inclusion_matrix[i, j] == 1) {
-                included_predictors[k] = predictors[j]
-                k = k + 1
-            }
-        }
-
-        # if there are no predictors, add a constant term
-        formula_string = "1"
-        if (length(included_predictors) > 0) {
-            formula_string = paste(included_predictors, collapse="+")
-        }
-        formula_string = paste(response_var, "~", formula_string)
-        
-        # train the model
-        model = lm(formula=formula_string, data=dataframe)
-        predictor_matrix_row = inclusion_matrix[j,]
-        models[[i]] = c(predictor_matrix_row, model)
+        # train k-fold number of models
+        model_data[[i]] = k_fold_cv_train_lm(inclusion_matrix[i,], dataframe, num_folds)
     }
-    return(models)
+    return(model_data)
+}
+
+# finds MSE for linear_model on test_data, assuming first column is response
+# TODO: test
+test_lm = function(linear_model, test_data) {
+    predict_response = predict(linear_model, test_data)
+    true_response = test_data[,1]
+    num_samples = length(true_response)
+    mse = (predict_response - true_response)^2 / num_samples
+    return(mse)
+}
+
+# returns statistics for a single linear model
+# TODO finish and test
+get_individual_statistics = function(linear_model) {
+    summary_obj = summary(linear_model)
+    statistics = list(
+        # could use summary_obj$df instead
+        "num_training_samples"=(linear_model$df.residual + linear_model$rank),
+        "num_coefficients"=linear_model$rank,
+        "r_squared"=summary_obj$r.squared,
+        "r_squared_adjusted"=summary_obj$adj.r.squared,
+
+        # VIF needs package
+        )
+    return(statistics)
+}
+
+# TODO incomplete, requires graph of adjacent parameter subsets
+get_paired_statistics = function(lm_subset, lm_subset_statistics, lm_superset, lm_superset_statistics) {
+    # significance of added parameter: needs to know
+    # which parameter is added
+    # use summary_obj$coefficients
+
+    # ANOVA significance
+
+    # AIC and BIC relative
+    
+    # relative values of individual statistics
 }
 
 # tests loading data, then generating linear models as data to input into meta model
@@ -172,11 +231,16 @@ generate_lms = function() {
     for (filename in filenames) {
         dataset = load_dataframe(DATA_PATH, filename)
 
-        models = fit_linear_models(dataset)
-        linear_models = append(linear_models, models)
-        print(summary(linear_models))
+        models_and_data = fit_linear_models(dataset, NUM_FOLDS)
+        linear_models = append(linear_models, models_and_data)
     }
     return(linear_models)
+}
+
+# TODO: call this after linear models generated
+# need graph of adjacent models
+generate_lm_statistics = function(linear_model_table) {
+
 }
 
 # only need to run the following once to get data into files
@@ -184,3 +248,5 @@ generate_lms = function() {
 
 # where everything starts
 lms = generate_lms()
+
+generate_lm_statistics(lms)
